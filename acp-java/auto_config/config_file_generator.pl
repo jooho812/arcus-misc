da5f172195bf53f683c8889da9c3674e0a40ce5c
qeist <qeist@jam2in.com>
2015-08-13 14:42:01 +0900

    file delimeter/comment process  modified.

--- acp-java/auto_config/config_file_generator.pl
+++ acp-java/auto_config/config_file_generator.pl
@@ -8,6 +8,9 @@ my %config_list = ();   # Hash   : config_file
 my @config_order = ();  # Array  : only key
 my @arr_temp = ();      # Array  : temporary key, value
 
+my $is_first = 1;
+my $filename;
+
 ########################################
 # Sub routines ...
 ########################################
@@ -25,29 +28,41 @@ sub check_param  {
 	}
 }
 
-sub file_read {
+sub file_read_default_config {
 	my $default = $ARGV[0];
-	my $test_description = $ARGV[1];
 
 	# FIXME Is argv file exists ? : exception
 	# Read $default configure File.
-	open (FP, "< $default") || die "cannot open file :  $default"; 
-	while (my $line = <FP>) {
+	open (FPD, "< $default") || die "cannot open file :  $default"; 
+	while (my $line = <FPD>) {
 		# remove '\n' by chomp function.
 		chomp($line);
 
+		# Ignore comment.
+		if (substr($line, 0, 1) eq "#") {
+			next;
+		}
+
+
 		@arr_temp = split('=', $line);
 		
-		# add to %config_list
+		# Add to Hash %config_list
 		$config_list{$arr_temp[0]} = "$arr_temp[1]";
 
-
+		# Add to Array @config_order 
 		push @config_order, $arr_temp[0];
-		#print "===================================\n";
-		#print "test1 : " . $arr_temp[0] . "\n";
-		#print "test2 : " . $arr_temp[1] . "\n";
 	}
-	close FP;
+	close FPD;
+}
+
+sub reset_default_config {
+	%config_list = ();
+	@config_order = ();
+}
+
+sub file_read {
+
+	my $test_description = $ARGV[1];
 
 	# second file.
 	# Read Test-Description File.
@@ -55,25 +70,40 @@ sub file_read {
 	my $bool = 0;
 	open (FP, "< $test_description") || die "cannot open file :  $test_description"; 
 	while (my $line = <FP>) {
-
 		chomp($line);
 
-		if ($line eq "") {
-			# Call file_write()
-			file_write($new_filename);
+		# Ignore comment.
+		if (substr($line, 0, 1) eq "#") {
 			next;
 		}
 
+		if ($line eq "") {
+			next;
+		}
 
 		# rex : \[.*\]
 		# Check startswith '[' and endswith ']'
 		if ($line =~ m/\[.*\]/) {
+
+			$filename = $new_filename;
 			$new_filename = substr $line, 1, -1;
+
+			if ($is_first == 1) {
+				$is_first = 0;
+			} elsif ($is_first == 0) {
+				# Call file_write()
+				file_write($filename);
+			}
+
+			# Read default_setting_file.
+			reset_default_config();
+			file_read_default_config();
+
 		}
 		else {
 			@arr_temp = split('=', $line);
 
-			# Check 
+			# Check element. Does element exists in @config_order?
 			$bool = 0;
 			for my $element (@config_order) {
 				if ($element eq $arr_temp[0]) {
@@ -86,8 +116,10 @@ sub file_read {
 
 			}
 			else {
+				print "called..\n";
 				push @config_order, $arr_temp[0];
 			}
+
 			$config_list{$arr_temp[0]} = "$arr_temp[1]";
 		}
 	}
@@ -101,14 +133,17 @@ sub file_write {
 	my $new_filename = shift @_;
 
 	open(FFP, ">", $new_filename) || die "Couldn't open '".$new_filename."' for writing because: ".$!;
+	print FFP "########################################\n";
+	print FFP "# default_config_file + $new_filename\n";
+	print FFP "########################################\n\n";
 
 	for my $key (@config_order) {
-		print FFP "$key" . "=" . "$config_list{$key}\n\n";
+		print FFP "$key=$config_list{$key}\n\n";
 	}
 
 	close FFP;
 
-	print "$new_filename generated...\n"
+	print "$new_filename(default_config_file + $new_filename) generated...\n"
 }
 
 
