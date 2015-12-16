1c8a18c4faad46eafc88481af52be8df802b06d6
yskimcau <yskimcau@gmail.com>
2015-12-16 19:46:36 +0900

    add feature : do not kill master in async change the method of starting memcached

--- acp-java/killandrun.memcached.bash
+++ acp-java/killandrun.memcached.bash
@@ -26,7 +26,8 @@ DIR=`dirname $DIR`
 MEMC_DIR_NAME=arcus-memcached
 MEMC_DIR=$DIR/../../$MEMC_DIR_NAME
 thread_count=32
-sleep_time=5
+sleep_time=3
+do_not_kill_async_master=1
 
 touch $MEMC_DIR_NAME.log
 
@@ -36,15 +37,23 @@ if [ "$kill_type" != "NONE" ];
 then
   if [ -f "pidfiles/memcached.127.0.0.1:$port_num" ];
   then
+    while [ $do_not_kill_async_master -eq 1 ];
+    do
+      is_master=`echo "stats replication" | nc 127.0.0.1 $port_num | grep "mode master" | wc -l`
+      if [ "$is_master" == "0" ];
+      then
+        break
+      fi
+      is_async=`echo "stats replication" | nc 127.0.0.1 $port_num | grep "state ASYNC_RUN" | wc -l`
+      if [ "$is_async" == "0" ];
+      then
+        break
+      fi
+      echo ">>>>>> memcached.127.0.0.1:$port_num is master but in async... Please wait a second.."
+      sleep 1
+    done
     echo ">>>>>> kill -$kill_type `cat pidfiles/memcached.127.0.0.1:$port_num`"
     kill -$kill_type `cat pidfiles/memcached.127.0.0.1:$port_num`
-    if [ "$kill_type" == "KILL" ];
-    then
-      sleep_time=40
-    fi
-    echo ">>>>>> wait a second to terminate $pidfile : `date`"
-    echo ">>>>>> sleep for $sleep_time"
-    sleep $sleep_time
   fi
 fi
 
@@ -53,9 +62,21 @@ sleep 1
 
 USE_SYSLOG=1
 
-if [ $USE_SYSLOG -eq 1 ];
-then
-  $MEMC_DIR/memcached -E $MEMC_DIR/.libs/default_engine.so -X $MEMC_DIR/.libs/syslog_logger.so -X $MEMC_DIR/.libs/ascii_scrub.so -d -v -r -R5 -U 0 -D: -b 8192 -m2000 -p $port_num -c 1000 -t $thread_count -z 127.0.0.1:2181 -e "replication_config_file=replication.config;" -P pidfiles/memcached.127.0.0.1:$port_num -o 3 -g 100
-else
-  $MEMC_DIR/memcached -E $MEMC_DIR/.libs/default_engine.so -X $MEMC_DIR/.libs/ascii_scrub.so -d -v -r -R5 -U 0 -D: -b 8192 -m2000 -p $port_num -c 1000 -t $thread_count -z 127.0.0.1:2181 -e "replication_config_file=replication.config;" -P pidfiles/memcached.127.0.0.1:$port_num -o 3 -g 100 >> $MEMC_DIR_NAME.log 2>&1
-fi
+while :
+do
+  if [ $USE_SYSLOG -eq 1 ];
+  then
+    $MEMC_DIR/memcached -E $MEMC_DIR/.libs/default_engine.so -X $MEMC_DIR/.libs/syslog_logger.so -X $MEMC_DIR/.libs/ascii_scrub.so -d -v -r -R5 -U 0 -D: -b 8192 -m1000 -p $port_num -c 1000 -t $thread_count -z 127.0.0.1:2181 -e "replication_config_file=replication.config;" -P pidfiles/memcached.127.0.0.1:$port_num -o 3 -g 100
+  else
+    $MEMC_DIR/memcached -E $MEMC_DIR/.libs/default_engine.so -X $MEMC_DIR/.libs/ascii_scrub.so -d -v -r -R5 -U 0 -D: -b 8192 -m1000 -p $port_num -c 1000 -t $thread_count -z 127.0.0.1:2181 -e "replication_config_file=replication.config;" -P pidfiles/memcached.127.0.0.1:$port_num -o 3 -g 100 >> $MEMC_DIR_NAME.log 2>&1
+  fi
+
+  sleep $sleep_time
+  is_run=`echo "stats replication" | nc 127.0.0.1 $port_num | grep "STAT mode" | wc -l`
+  if [ "$is_run" == "1" ];
+  then
+    break
+  fi
+  echo ">>>>>> memcached.127.0.0.1:$port_num is not ready to run... Please wait a second..."
+  #sleep 1
+done
