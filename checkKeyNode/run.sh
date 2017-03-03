14107ae8f5416676bd86fdb02599209ce83df5ed
Wookhwan, Choi <whchoi83@jam2in.com>
2017-03-03 17:51:23 +0900

    fixed jar version error

--- checkKeyNode/run.sh
+++ checkKeyNode/run.sh
@@ -4,4 +4,4 @@ then
     mvn dependency:copy-dependencies
 fi
 
-java -classpath "./target/classes:./target/dependency/arcus-java-client-1.9.4.jar:./target/dependency/ehcache-core-2.6.0.jar:./target/dependency/zookeeper-3.4.5.jar:./target/dependency/jline-0.9.94.jar:./target/dependency/junit-3.8.1.jar:./target/dependency/netty-3.2.2.Final.jar:./target/dependency/log4j-1.2.16.jar:./target/dependency/slf4j-api-1.6.1.jar:./target/dependency/slf4j-log4j12-1.6.1.jar" CheckKeyNode
+java -classpath "./target/classes:./target/dependency/arcus-java-client-1.9.6.jar:./target/dependency/ehcache-core-2.6.0.jar:./target/dependency/zookeeper-3.4.5.jar:./target/dependency/jline-0.9.94.jar:./target/dependency/junit-3.8.1.jar:./target/dependency/netty-3.2.2.Final.jar:./target/dependency/log4j-1.2.16.jar:./target/dependency/slf4j-api-1.6.1.jar:./target/dependency/slf4j-log4j12-1.6.1.jar" CheckKeyNode
